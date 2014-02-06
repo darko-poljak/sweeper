5040c33f96a7dac7fc10e78c3fedc0549d07f49d
Darko Poljak <darko.poljak@gmail.com>
2014-02-06 14:43:32 +0100

    added file_dups_immediate function

--- sweeper/__init__.py
+++ sweeper/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
-from .sweeper import file_dups, mv_file_dups, rm_file_dups, iter_file_dups
+from .sweeper import file_dups, mv_file_dups, rm_file_dups, iter_file_dups,\
+    file_dups_immediate
 
-__all__ = ['file_dups', 'mv_file_dups', 'rm_file_dups', 'iter_file_dups']
+__all__ = ['file_dups', 'mv_file_dups', 'rm_file_dups', 'iter_file_dups',
+           'file_dups_immediate']
