4a45525d00a59e4cc96cb95d527b644cc13be3e4
Darko Poljak <darko.poljak@gmail.com>
2014-02-07 08:30:18 +0100

    cleanups and version inc

--- sweeper/__init__.py
+++ sweeper/__init__.py
@@ -1,6 +1,4 @@
 from __future__ import absolute_import
-from .sweeper import file_dups, mv_file_dups, rm_file_dups, iter_file_dups,\
-    file_dups_immediate
+from .sweeper import file_dups, mv_file_dups, rm_file_dups, iter_file_dups
 
-__all__ = ['file_dups', 'mv_file_dups', 'rm_file_dups', 'iter_file_dups',
-           'file_dups_immediate']
+__all__ = ['file_dups', 'mv_file_dups', 'rm_file_dups', 'iter_file_dups']
